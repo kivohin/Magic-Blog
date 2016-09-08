af1e6e183dab6a38a62ed7cb0d425fa3c0e89c83
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-08 11:40:17 -0400

    Migrate User model into DB.

--- web_blog/db/schema.rb
+++ web_blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907143320) do
+ActiveRecord::Schema.define(version: 20160908153053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20160907143320) do
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
