f15ecf839f97bfbcaf60c89a17cbe58589db53b3
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-08 14:36:10 -0400

    Add login route.

--- web_blog/config/routes.rb
+++ web_blog/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
 
+  get 'login' => 'sessions#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
