2cde166aa8575c214caaf8838c5ae281d47c9abc
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-08 14:07:29 -0400

    Add create action to Users controller.

--- web_blog/app/controllers/users_controller.rb
+++ web_blog/app/controllers/users_controller.rb
@@ -3,8 +3,18 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to '/'
+		else
+			redirect_to '/signup'
+		end
+	end
+
 	private
-	def users_params
+	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
 end
