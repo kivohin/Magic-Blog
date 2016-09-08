436c7fae6e40a49a2ff563d171c5a9786be6f3e7
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-08 13:56:31 -0400

    Add private section to Users controller where I create an action which verifies that data submitted via form are secure.

--- web_blog/app/controllers/users_controller.rb
+++ web_blog/app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	private
+	def users_params
+		params.require(:user).permit(:username, :email, :password)
+	end
 end
