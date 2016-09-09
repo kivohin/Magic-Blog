5b905ca9fbe67612bf9a26c102cdba7a8c88c899
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-09 13:45:28 -0400

    Add CRUD actions for Posts controller. Create relations between DB's.

--- web_blog/app/controllers/sessions_controller.rb
+++ web_blog/app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
 	def create
 		@user = User.find_by_email(params[:session][:email])
+		
 		if @user && @user.authenticate(params[:session][:password])
 			session[:user_id] = @user.id
 			redirect_to '/'
--- web_blog/app/controllers/users_controller.rb
+++ web_blog/app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
+		
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to '/'
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
 	end
 
 	private
+	
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
