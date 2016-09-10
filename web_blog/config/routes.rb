a74281fd6660bae98b70e61fd2d8a7efad0c9699
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-09 21:30:37 -0400

    Add require_user method in application controller. Fixed has_many associations in user model. Generate layout for index and show views. Add routes for posts actions, and define root path.

--- web_blog/config/routes.rb
+++ web_blog/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   get 'signup' => 'users#new'
   resources :users
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
