058e55686ac3f2a5389692ba36ee51fa45947ff9
Anthony Nabuurs <kivohin@protonmail.com>
2016-09-11 12:54:46 -0400

    Add padding to text inside form fields. Change save procedure when creating from form in comments controller. In comments form restricted current user's username value.

--- web_blog/app/controllers/comments_controller.rb
+++ web_blog/app/controllers/comments_controller.rb
@@ -3,8 +3,13 @@ class CommentsController < ApplicationController
 
 	def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to posts_path(@post)
+    @comment = @post.comments.new(comment_params)
+
+    if @comment.save
+    	redirect_to post_path(@post)
+    else
+    	redirect_to '/'
+    end
   end
  
   private
